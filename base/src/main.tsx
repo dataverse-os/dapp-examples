31f74c8ab6c75c78a9e53d84872e99da42ba26b6
Canvas Lee <746591811@qq.com>
2023-07-20 18:34:24 +0800

    feat: delete hooks, types and utils

--- base/src/main.tsx
+++ base/src/main.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ConfigProvider } from "./context/configContext";
-import { UserProvider } from "./context/userContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ConfigProvider>
-    <UserProvider>
-        <App />
-    </UserProvider>
-  </ConfigProvider>
+  <App />
 );
